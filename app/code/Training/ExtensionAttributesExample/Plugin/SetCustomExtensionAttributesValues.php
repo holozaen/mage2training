ebfde484e42ea748380ef4e9f29b2dadff02383b
Peter Baettig <info@online-verkaufen.ch>
2016-11-18 11:32:54 +0100

    Extension Attributes Example (incomplete)

---
+++ app/code/Training/ExtensionAttributesExample/Plugin/SetCustomExtensionAttributesValues.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ovc
+ * Date: 18.04.16
+ * Time: 17:24
+ */
+
+namespace Training\ExtensionAttributesExample\Plugin;
+
+
+class SetCustomExtensionAttributesValues
+{
+
+}
