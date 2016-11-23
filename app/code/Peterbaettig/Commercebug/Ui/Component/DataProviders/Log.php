6c8194bdce65f539e6bc9b5bb25b4412410a6a85
Peter Baettig <info@online-verkaufen.ch>
2016-11-23 09:46:21 +0100

    Commercebug

---
+++ app/code/Peterbaettig/Commercebug/Ui/Component/DataProviders/Log.php
@@ -0,0 +1,19 @@
+<?php
+namespace Peterbaettig\Commercebug\Ui\Component\DataProviders;
+use \Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider;
+use Magento\Ui\DataProvider\AbstractDataProvider;
+
+class Log extends AbstractDataProvider
+{
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        \Peterbaettig\Commercebug\Model\ResourceModel\Log\CollectionFactory $collectionFactory,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->collection = $collectionFactory->create();
+    }
+}
