381e63aebe905f6fa936ffcd459d56367a52b78f
Peter Baettig <info@online-verkaufen.ch>
2016-11-28 16:34:02 +0100

    Add basic form with data binding

---
+++ app/code/Ovc/Customtags/Ui/DataProvider/EditDataProvider.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ovc
+ * Date: 24.11.16
+ * Time: 17:37
+ */
+
+namespace Ovc\Customtags\Ui\DataProvider;
+
+
+use Magento\Ui\DataProvider\AbstractDataProvider;
+use Magento\Ui\DataProvider\Modifier\ModifierInterface;
+use Magento\Ui\DataProvider\Modifier\PoolInterface;
+use Ovc\Customtags\Model\ResourceModel\Tag\CollectionFactory;
+
+class EditDataProvider extends AbstractDataProvider
+{
+    /**
+     * @var array
+     */
+    protected $_loadedData;
+
+
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->collection = $collectionFactory->create();
+    }
+
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        if (isset($this->_loadedData)) {
+            return $this->_loadedData;
+        }
+        $items = $this->collection->getItems();
+        foreach ($items as $item) {
+            $this->_loadedData[$item->getId()] = $item->getData();
+        }
+        return $this->_loadedData;
+    }
+
+}
