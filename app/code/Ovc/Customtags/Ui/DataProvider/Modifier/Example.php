d38c39919d80ea6c9e2af58a2e22a4a10da0f59d
Peter Baettig <info@online-verkaufen.ch>
2016-11-28 17:39:04 +0100

    Modifier for product edit form - works

---
+++ app/code/Ovc/Customtags/Ui/DataProvider/Modifier/Example.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ovc
+ * Date: 28.11.16
+ * Time: 16:36
+ */
+
+namespace Ovc\Customtags\Ui\DataProvider\Modifier;
+
+
+use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\AbstractModifier;
+
+class Example extends AbstractModifier
+{
+    public function modifyMeta(array $meta)
+    {
+        $meta['test_fieldset_name'] = [
+            'arguments' => [
+                'data' => [
+                    'config' => [
+                        'componentType' => 'fieldset',
+                        'label' => __('Label For Fieldset'),
+                        'sortOrder' => 50,
+                        'collapsible' => true
+                    ]
+                ]
+            ],
+            'children' => [
+                'test_field_name' => [
+                    'arguments' => [
+                        'data' => [
+                            'config' => [
+                                'formElement' => 'select',
+                                'componentType' => 'field',
+                                'options' => [
+                                    ['value' => 'test_value_1', 'label' => 'Test Value 1'],
+                                    ['value' => 'test_value_2', 'label' => 'Test Value 2'],
+                                    ['value' => 'test_value_3', 'label' => 'Test Value 3'],
+                                ],
+                                'visible' => 1,
+                                'required' => 1,
+                                'label' => __('Label For Element')
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        return $meta;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function modifyData(array $data)
+    {
+        return $data;
+    }
+}
